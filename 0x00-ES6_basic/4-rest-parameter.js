fec0da1341208134d8b2d0669727473e7b75d9a0
BlightG <birhane.gulilat@gmail.com>
2023-04-18 16:52:23 +0300

    commit for task 4 and 8

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...theArgs) {
   let total = 0;
-  for (const arg of theArgs) {
+  for (let i = 0; i < theArgs.length; i += 1) {
     total += 1;
   }
   return total;
