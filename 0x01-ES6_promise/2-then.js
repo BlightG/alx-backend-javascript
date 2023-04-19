1ff806fb5a3e1add067cb6c316434edad4ce187c
BlightG <birhane.gulilat@gmail.com>
2023-04-19 19:18:58 +0300

    task 2 first |=|)

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    console.log('Got a response from the API');
+    if (promise) {
+      resolve(
+        {
+          status: 200,
+          body: 'success',
+        },
+      );
+    } else {
+      reject(Error());
+    }
+  });
+}
