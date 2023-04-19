5723e08efd8abc84d7eff8a9cccbe3a2d4753c40
BlightG <birhane.gulilat@gmail.com>
2023-04-19 20:34:31 +0300

    removed try cathc

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  try {
-    uploadPhoto().then(
-      (value) => {
-        const body = value;
-        createUser().then(
-          (value) => { console.log(`${body.body} ${value.firstName} ${value.lastName}`); },
-        );
-      },
-    );
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+  // try {
+  uploadPhoto().then(
+    (value) => {
+      const body = value;
+      createUser().then(
+        (value) => { console.log(`${body.body} ${value.firstName} ${value.lastName}`); },
+      );
+    },
+  );
+  // } catch (error) {
+  //   console.log('Signup system offline');
+  // }
 }
