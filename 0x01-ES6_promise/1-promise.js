454d78cf9b5dcbcdc5cd8adf19dd4ae07f159b78
BlightG <birhane.gulilat@gmail.com>
2023-04-19 17:10:04 +0300

    lint fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,8 +7,7 @@ export default function getFullResponseFromAPI(success) {
           body: 'Success',
         },
       );
-    }
-    else {
+    } else {
       reject(Error('The fake API is not working currently'));
     }
   });
