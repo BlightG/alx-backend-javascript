11383b22b743ddfc58d8105812a3edd7a6eec624
BlightG <birhane.gulilat@gmail.com>
2023-04-19 20:31:51 +0300

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // const bodytag = uploadPhoto();
-  // const user = createUser();
-  // console.log(bodytag.body);
-  // console.log(user)
   try {
     uploadPhoto().then(
       (value) => {
-        const body = value.body;
+        const body = value;
         createUser().then(
-          (value) => { console.log(`${body} ${value.firstName} ${value.lastName}`); },
+          (value) => { console.log(`${body.body} ${value.firstName} ${value.lastName}`); },
         );
-      });
+      },
+    );
   } catch (error) {
     console.log('Signup system offline');
   }
