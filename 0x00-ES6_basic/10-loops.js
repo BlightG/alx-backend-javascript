ce21a351df7df6210a37c974063cb246a047a229
BlightG <birhane.gulilat@gmail.com>
2023-04-19 10:40:36 +0300

    reinstated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-//   const newarray = [];
+  const newarray = [];
   for (const idx of array.keys()) {
     const value = array[idx];
-    array[idx] = appendString + value;
+    newarray[idx] = appendString + value;
   }
 
-  return array;
+  return newarray;
 }
