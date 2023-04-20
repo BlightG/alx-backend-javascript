ea177cf296edcaa39fa9ea7aee9061411ed1c665
BlightG <birhane.gulilat@gmail.com>
2023-04-20 15:17:10 +0300

    task 2 attempt lint missing

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  // cosntruct all value with variable names makrked as private
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw 'Name must be a string';
+    }
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw 'lenght must be a Number';
+
+    }
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    } else {
+      throw 'students must be an Array of strings';
+    }
+  }
+}
