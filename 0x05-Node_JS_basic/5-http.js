d52ff49955a0cd3eae390b4a691b23837f019785
BlightG <birhane.gulilat@gmail.com>
2023-06-12 16:40:35 +0300

    changes

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  }
+
+  if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students');
+    const output = await countStudents(process.argv[2]);
+    res.write(JSON.stringify(output));
+    res.end()
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log('...')
+});
+
+module.exports = app
