7d1cab0c361a91047739f62bba58cd9146a73ab8
BlightG <birhane.gulilat@gmail.com>
2023-06-13 14:25:25 +0300

    task 3 good attempt

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,46 +1,42 @@
 const fs = require('fs');
-const csv = require("csv-parse");
-const { rejects } = require('assert');
-const results = [];
-const field = {};
+const readline = require('readline');
+const field = {}
 
-async function readcsv(path) {
-  return new Promise((resolve, reject) => {
-    let rowcount = 0;
-    fs.createReadStream(path)
-      .pipe(csv.parse({ headers: false }))
-      .on("data", (data) => {
-        if (rowcount != 0) {
-          if (data[3] in field) {
-            field[data[3]].push(data[0]);
-          } else {
-            const studentlist = [];
-            field[data[3]] = studentlist;
-            studentlist.push(data[0]);
-          }
-          results.push(field);
-        }
-        rowcount++;
-      })
-      .on("end", () => {
-        console.log(`Number of students: ${results.length}`)
-        for (key in field) {
-          console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `)
-        }
-        resolve();
+function countStudents(path) {
+  if (fs.existsSync(path)) {
+    return new Promise((resolve, reject) => {
+      let rowcount = 0;
+      const inputstream = fs.createReadStream(path);
+      let linereader = readline.createInterface({
+        input: inputstream,
+        terminal: false,
       });
-  });
-}
-
-async function countStudents(path) {
-  try {
-    if (fs.existsSync(path)) {
-      return readcsv(path).then(() => Promise.resolve());
-    } else {
-      throw new Error('Cannot load the database');
-    }
-  } catch (err) {
-    console.error(err);
+      linereader
+        .on('line', (line) => {
+          if (line.length > 0) {
+            if (rowcount != 0) {
+              const val = line.split(',');
+              if (val[3] in field) {
+                field[val[3]].push(val[0]);
+              } else {
+                let studentlist = [];
+                studentlist.push(val[0]);
+                field[val[3]] = studentlist;
+              }
+            }
+            rowcount++;
+          }
+        })
+        .on("close", () => {
+          console.log(`Number of students: ${rowcount - 1}`);
+          for (key in field) {
+            console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `);
+          }
+          resolve()
+        });
+    });
+  } else {
+    throw new Error('Cannot load the database');
   }
 }
 
