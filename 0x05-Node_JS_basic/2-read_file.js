459e7719d527a9e8e2a060ea2e3d7093f446f363
BlightG <birhane.gulilat@gmail.com>
2023-06-13 12:48:03 +0300

    redid task 2 with readline

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
-const csv = require("csv-parse");
-const results = [];
-const field = {};
-
-function readcsv(path) {
-  let rowcount = 0;
-  fs.createReadStream(path)
-    .pipe(csv.parse({ headers: false }))
-    .on("data", (data) => {
-      if (rowcount != 0) {
-        if (data[3] in field) {
-          field[data[3]].push(data[0]);
-        } else {
-          const studentlist = [];
-          field[data[3]] = studentlist;
-          studentlist.push(data[0]);
-        }
-        results.push(field);
-      }
-      rowcount++;
-    })
-    .on("end", () => {
-      console.log(`Number of students: ${results.length}`)
-      for (key in field) {
-        console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `)
-      }
-    });
-}
+const readline = require('readline');
+const field = {}
 
 function countStudents(path) {
   try {
     if (fs.existsSync(path)) {
-      readcsv(path)
+      let rowcount = 0;
+      const inputstream = fs.createReadStream(path);
+      let linereader = readline.createInterface({
+        input: inputstream,
+        terminal: false,
+      });
+      linereader
+        .on('line', (line) => {
+          if (rowcount != 0) {
+            const val = line.split(',');
+            if (val[3] in field) {
+              field[val[3]].push(val[0]);
+            } else {
+              let studentlist = [];
+              studentlist.push(val[0]);
+              field[val[3]] = studentlist;
+            }
+          }
+          rowcount++;
+        })
+        .on("close", () => {
+          console.log(`Number of students: ${rowcount - 1}`);
+          for (key in field) {
+            console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `);
+          }
+        });
     } else {
       throw new Error('Cannot load the database');
     }
