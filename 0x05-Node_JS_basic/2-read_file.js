5e89a05d881fcc9a8a74c870ed3c4a6287003a24
BlightG <birhane.gulilat@gmail.com>
2023-06-13 13:31:17 +0300

    done using fs.readFileSync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,29 @@
 const fs = require('fs');
-const readline = require('readline');
-const field = {}
+const field = {};
 
 function countStudents(path) {
   if (fs.existsSync(path)) {
     let rowcount = 0;
-    const inputstream = fs.createReadStream(path);
-    let linereader = readline.createInterface({
-      input: inputstream,
-      terminal: false,
-    });
-    linereader
-      .on('line', (line) => {
-        if (line.length > 0) {
-          if (rowcount != 0) {
-            const val = line.split(',');
-            if (val[3] in field) {
-              field[val[3]].push(val[0]);
-            } else {
-              let studentlist = [];
-              studentlist.push(val[0]);
-              field[val[3]] = studentlist;
-            }
+    const allFileContents = fs.readFileSync(path, 'utf-8');
+    allFileContents.split(/\r?\n/).forEach(line => {
+      if (line.length > 0) {
+        if (rowcount != 0) {
+          const val = line.split(',');
+          if (val[3] in field) {
+            field[val[3]].push(val[0]);
+          } else {
+            let studentlist = [];
+            studentlist.push(val[0]);
+            field[val[3]] = studentlist;
           }
-          rowcount++;
         }
-      })
-      .on("close", () => {
-        console.log(`Number of students: ${rowcount - 1}`);
-        for (key in field) {
-          console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")}`);
-        }
-      });
+        rowcount++;
+      }
+    });
+    console.log(`Number of students: ${rowcount - 1}`);
+    for (key in field) {
+      console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")}`);
+    }
   } else {
     throw new Error('Cannot load the database');
   }
