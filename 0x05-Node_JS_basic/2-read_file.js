87b32092c7cb17b1ec1f6b78a22b857cfe72a78f
BlightG <birhane.gulilat@gmail.com>
2023-06-13 12:52:04 +0300

    removed try cathc

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,40 +3,40 @@ const readline = require('readline');
 const field = {}
 
 function countStudents(path) {
-  try {
-    if (fs.existsSync(path)) {
-      let rowcount = 0;
-      const inputstream = fs.createReadStream(path);
-      let linereader = readline.createInterface({
-        input: inputstream,
-        terminal: false,
+  // try {
+  if (fs.existsSync(path)) {
+    let rowcount = 0;
+    const inputstream = fs.createReadStream(path);
+    let linereader = readline.createInterface({
+      input: inputstream,
+      terminal: false,
+    });
+    linereader
+      .on('line', (line) => {
+        if (rowcount != 0) {
+          const val = line.split(',');
+          if (val[3] in field) {
+            field[val[3]].push(val[0]);
+          } else {
+            let studentlist = [];
+            studentlist.push(val[0]);
+            field[val[3]] = studentlist;
+          }
+        }
+        rowcount++;
+      })
+      .on("close", () => {
+        console.log(`Number of students: ${rowcount - 1}`);
+        for (key in field) {
+          console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `);
+        }
       });
-      linereader
-        .on('line', (line) => {
-          if (rowcount != 0) {
-            const val = line.split(',');
-            if (val[3] in field) {
-              field[val[3]].push(val[0]);
-            } else {
-              let studentlist = [];
-              studentlist.push(val[0]);
-              field[val[3]] = studentlist;
-            }
-          }
-          rowcount++;
-        })
-        .on("close", () => {
-          console.log(`Number of students: ${rowcount - 1}`);
-          for (key in field) {
-            console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(", ")} `);
-          }
-        });
-    } else {
-      throw new Error('Cannot load the database');
-    }
-  } catch (err) {
-    console.error(err);
+  } else {
+    throw new Error('Cannot load the database');
   }
+  // } catch (err) {
+  //   console.error(err);
+  // }
 }
 
 module.exports = countStudents;
