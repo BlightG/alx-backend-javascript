13a28c3f4296eb968faea0118a2ae90226972831
BlightG <birhane.gulilat@gmail.com>
2023-06-16 21:16:55 +0300

    added callback

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -9,6 +9,8 @@ app.use('/', router);
 app.use('/students', router);
 app.use('/students/:major', router);
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Serever running on port ${port}`)
+});
 
 export default app;
